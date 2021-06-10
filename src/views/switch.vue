9245798b5e4f62e4ea58b0ca70f096d52aa8a47d
shiq <shiq@robooot.com>
2021-06-10 17:45:06 +0800

    校验文件

--- src/views/switch.vue
+++ src/views/switch.vue
@@ -1,1 +1,22 @@
-c3468acc45859a7669c8584f0fb70450
+<style>
+</style>
+
+<template>
+    <div class="app-container">
+    </div>
+   
+</template>
+<script>
+import { mapGetters } from "vuex";
+export default {
+    data() {
+        return {
+        };
+    },
+    created() {
+        window.location.href="https://appzkXRI3fX8494.h5.xiaoeknow.com/v2/course/alive/l_60c03e3de4b0c726421d9fc1?app_id=appzkXRI3fX8494&alive_mode=0&pro_id=&type=2"
+    },
+    methods: {
+    }
+};
+</script>
